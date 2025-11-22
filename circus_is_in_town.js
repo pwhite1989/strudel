c698c85daeeea5b46a941680fdbed01bae49c839
Patrick White <15720524+pwhite1989@users.noreply.github.com>
2025-11-22 19:54:01 +0000

    Refactor circus_is_in_town.js for pattern and chords

--- circus_is_in_town.js
+++ circus_is_in_town.js
@@ -1,20 +1,20 @@
-// "Circus is in town"
+// "Pixelated Castle"
 // @by Paddy
 
 let chords = chord(`<AbM CM DbM <CbM [CbM CbM7]>>`)
 
-pattern: "<~ 0 0 0 >/8".pickRestart([
+pattern: "<~ ~ 0 0 0 0 0 0>/8".pickRestart([
   n("7 [5 <[6 5 6] 6>] 2 [2 3 4]").set(chords).voicing().dec(.2) 
 ])
-chord_progression$: "<0 0 0 0>/8".pickRestart([
+chord_progression$: "<0 0 0 0 0 0 0 0>/8".pickRestart([
   chords.struct("- x - x").voicing().room(.5)
 ])
 
-blip: "<0 0 0 0 >/8".pickRestart([
+blip: "<0 0 0 0 0 0 0 0>/8".pickRestart([
   n("~@16 [0 - 1 -]").set(chords).mode("<below above duck root>:Cb6").voicing()
 ])
 
-bass: "<~ 0 0 0 >/8".pickRestart([
+bass: "<~ 0 0 0 0 0 0 0>/8".pickRestart([
   n("[0 ~ 1 ~]!3 [1 ~ 0 ~]")
   .slow(4)
   .add("<0 5 7 0>")
@@ -25,11 +25,16 @@ bass: "<~ 0 0 0 >/8".pickRestart([
   .sound("square")
 ])
 
-// This is just an example taken from "Bennington" by @eefano. This is just a reference on how to use pickOut
-drums: "<~ [0,1,2] 2  [0,2] [0,1,2] [0,2]@2 2@2 [2,[~ 1*2]] 2@2 [0,1,2] ~>/8".pickRestart([
-  "<[~ <~@3 bd ~@4>]>*4",  "<~ <cp ~> ~@6>*4",  "<bd sd>*4"
+drums: "<~ ~ 0 1 [0,1,2] [1,2] [0,2,4] 0>/8".pickRestart([
+  "<~@7 [~ ~ ~ cp]>", 
+  "<bd sd>*4",
+  "<[rd rd [rd rd] rd rd [rd rd] rd rd]>",
+  "<[~ <~@3 bd ~@4>]>*4",
+  "<[bd@2 [~@0.1 bd bd]] sd [~ bd]>*4",
+  "<sd>*4"
 ]).pickOut({
   bd:s('linndrum_bd').lpf(3000).room(.25).gain(.75),
   sd:s('linndrum_sd').room(.25).gain(.65),
-  cp:s('cp').velocity(.65).room(.8)
+  cp:s('cp').velocity(.65).room(.8),
+  rd:s('rd').gain("<[1 .75 [0.5 1] .75 1 [.75 .5] .75 1]>")
 })
